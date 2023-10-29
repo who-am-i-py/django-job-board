f254fee87e94d91825cd66f9f8a65c11593fbf5d
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-29 19:44:55 +0100

    pic field changes saving directory

--- job/models.py
+++ job/models.py
@@ -16,7 +16,7 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    pic = models.ImageField(upload_to='jobs/')
+    pic = models.ImageField(upload_to='job/job_pics')
 
     def __str__(self) :
         return f"[{self.title}]"
