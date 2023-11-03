7668045614cbba2a96a8b1065ce969bff5c491a7
who-am-i-py <sys.whoami.py@gmail.com>
2023-11-03 20:38:13 +0100

    Add pagination to list.html with condition

--- job/views.py
+++ job/views.py
@@ -5,11 +5,11 @@ from django.core.paginator import Paginator
 # Create your views here.
 def job_list(request):
     all_jobs = Job.objects.all()
-    paginator = Paginator(all_jobs, 1) 
+    paginator = Paginator(all_jobs, 4) 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {'jobs': page_obj}
-    return render(request, 'job/job/list.html',{'jobs': all_jobs})
+    return render(request, 'job/job/list.html',context)
 
 def job_detail(request, id):
     try:
